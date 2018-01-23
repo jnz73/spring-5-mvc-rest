298a33cfea6f9b1b54695ddaf0691faf60b05c88
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-01-23 15:39:49 +0100

    edit swagger metadata

--- src/main/java/it/gianni/spring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/it/gianni/spring5mvcrest/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,7 +23,21 @@ public class SwaggerConfig //extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Gianni Fabriziani", "www.google.com", "test@test.com");
+        return new ApiInfo(
+                "Spring Framework Guru Tutorial",
+                "Spring Framework Tutorial",
+                "1.0",
+                "blablabla",
+                contact,
+                "GPL",
+                "",
+                new ArrayList<>());
     }
 // MANUAL CONFIG OF SWAGGER GUI
 //    @Override
